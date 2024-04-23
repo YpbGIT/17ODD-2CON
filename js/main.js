a5d51904cc4d0db8358cf955af46bfcb380d8fc6
YpbGIT <Yann.pierre.belarbi@gmail.com>
2024-04-23 18:48:17 +0200

    banniere ajustée

--- js/main.js
+++ js/main.js
@@ -10,7 +10,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
 function loadCubeIframe(cubeName) {
     const cubeContainer = document.getElementById('cube-container');
-    const bannerSpace = document.getElementById('banner-space');
     
     // Supprime l'iframe précédent s'il existe
     const oldIframe = cubeContainer.querySelector('iframe');
@@ -19,24 +18,27 @@ function loadCubeIframe(cubeName) {
     }
 
     // Supprime la bannière précédente s'il existe
-    const oldBanner = bannerSpace.querySelector('img');
+    const oldBanner = document.querySelector('.banner-container');
     if (oldBanner) {
         oldBanner.remove();
     }
 
     // Crée une nouvelle bannière pour le cube spécifique
+    const bannerContainer = document.createElement('div');
+    bannerContainer.classList.add('banner-container');
     const banner = document.createElement('img');
-    banner.src = `img/Webbanderollesodd/b${cubeName.slice(3)}.jpg`; // Assurez-vous que le chemin est correct
-    banner.alt = `Bannière pour ${cubeName}`;
-    banner.classList.add('img-fluid'); // Ajoutez votre classe CSS pour le style de l'image
-    banner.style.width = '100%'; // La bannière prend toute la largeur
-    bannerSpace.appendChild(banner); // Ajoute la bannière au conteneur dédié
+    banner.src = `img/Webbanderollesodd/b${cubeName.slice(3)}.jpg`; // Chemin vers la bannière spécifique
+    banner.alt = `Bannière pour ${cubeName}`; // Texte alternatif pour la bannière
+    banner.style.width = '50%'; // Réduire la largeur de la bannière à 50%
+    bannerContainer.appendChild(banner);
+    cubeContainer.appendChild(bannerContainer);
 
     // Crée un nouvel iframe pour le cube spécifique
     const iframe = document.createElement('iframe');
-    iframe.src = `${cubeName}/index.html`; // Assurez-vous que le chemin est correct
+    iframe.src = `${cubeName}/index.html`; // Chemin vers l'index.html du cube spécifique
     iframe.style.width = '100%';
-    iframe.style.height = '100%'; // Prend toute la hauteur du conteneur de cube
-    iframe.frameBorder = '0';
-    cubeContainer.appendChild(iframe); // Ajoute l'iframe au conteneur de cube
+    iframe.style.height = '500px'; // Définissez la hauteur selon vos besoins
+    iframe.frameBorder = '0'; // Pour supprimer la bordure de l'iframe
+
+    cubeContainer.appendChild(iframe);
 }
